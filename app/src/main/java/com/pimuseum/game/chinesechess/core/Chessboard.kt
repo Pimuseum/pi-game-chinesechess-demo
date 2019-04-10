8f7d75a5d578ebc1ea3dc2e7d41c25f6ac062574
Jiervs <jiervsvirgil@gmail.com>
2019-04-10 14:05:56 +0800

    fix chessboard

--- app/src/main/java/com/pimuseum/game/chinesechess/core/Chessboard.kt
+++ app/src/main/java/com/pimuseum/game/chinesechess/core/Chessboard.kt
@@ -17,14 +17,6 @@ class Chessboard {
         private const val RowCapacity : Int = 10
     }
 
-//    private var chessmanList : ArrayList<Chessman?> = ArrayList(90)
-//
-//
-//    //获取当前棋盘上的棋子
-//    fun getCurrentChessmanList() : ArrayList<Chessman?> {
-//        return chessmanList
-//    }
-
     //棋盘上的二维坐标集信息,对应的索引取值是空值或者是棋子对象
     private var chessboardInfo : Array<Array<Chessman?>>
             = Array(RowCapacity){Array<Chessman?>(ColumnCapacity) { null } }
@@ -37,61 +29,60 @@ class Chessboard {
     //载入棋子
     fun loadChessman() {
 
-//        chessmanList.clear()
-//
-//        //红车
-//        chessmanList.add(JuChessman(ChessType.Red, Position(1, 1)))
-//        chessmanList.add(JuChessman(ChessType.Red, Position(1, 9)))
-//        //红马
-//        chessmanList.add(MaChessman(ChessType.Red, Position(1, 2)))
-//        chessmanList.add(MaChessman(ChessType.Red, Position(1, 8)))
-//        //红相
-//        chessmanList.add(XiangChessman(ChessType.Red, Position(1, 3)))
-//        chessmanList.add(XiangChessman(ChessType.Red, Position(1, 7)))
-//        //红士
-//        chessmanList.add(ShiChessman(ChessType.Red, Position(1, 4)))
-//        chessmanList.add(ShiChessman(ChessType.Red, Position(1, 6)))
-//        //帅
-//        chessmanList.add(KingChessman(ChessType.Red, Position(1, 5)))
-//        //红炮
-//        chessmanList.add(PaoChessman(ChessType.Red, Position(2, 2)))
-//        chessmanList.add(PaoChessman(ChessType.Red, Position(2, 8)))
-//        //兵
-//        chessmanList.add(PawnsChessman(ChessType.Red, Position(4, 1)))
-//        chessmanList.add(PawnsChessman(ChessType.Red, Position(4, 3)))
-//        chessmanList.add(PawnsChessman(ChessType.Red, Position(4, 5)))
-//        chessmanList.add(PawnsChessman(ChessType.Red, Position(4, 7)))
-//        chessmanList.add(PawnsChessman(ChessType.Red, Position(4, 9)))
-//
-//        //黑车
-//        chessmanList.add(JuChessman(ChessType.Black, Position(10, 1)))
-//        chessmanList.add(JuChessman(ChessType.Black, Position(10, 9)))
-//        //黑马
-//        chessmanList.add(MaChessman(ChessType.Black, Position(10, 2)))
-//        chessmanList.add(MaChessman(ChessType.Black, Position(10, 8)))
-//        //黑象
-//        chessmanList.add(XiangChessman(ChessType.Black, Position(10, 3)))
-//        chessmanList.add(XiangChessman(ChessType.Black, Position(10, 7)))
-//        //黑仕
-//        chessmanList.add(ShiChessman(ChessType.Black, Position(10, 4)))
-//        chessmanList.add(ShiChessman(ChessType.Black, Position(10, 6)))
-//        //将
-//        chessmanList.add(KingChessman(ChessType.Black, Position(10, 5)))
-//        //黑炮
-//        chessmanList.add(PaoChessman(ChessType.Black, Position(9, 2)))
-//        chessmanList.add(PaoChessman(ChessType.Black, Position(9, 8)))
-//
-//        //卒
-//        chessmanList.add(PawnsChessman(ChessType.Black, Position(7, 1)))
-//        chessmanList.add(PawnsChessman(ChessType.Black, Position(7, 3)))
-//        chessmanList.add(PawnsChessman(ChessType.Black, Position(7, 5)))
-//        chessmanList.add(PawnsChessman(ChessType.Black, Position(7, 7)))
-//        chessmanList.add(PawnsChessman(ChessType.Black, Position(7, 9)))
-    }
+        //清空棋盘坐标上的棋子
+        for(row in 1..RowCapacity) {
+            for (column in 1..ColumnCapacity) {
+                chessboardInfo[row][column] = null
+            }
+        }
 
-    //棋子被吃
-    fun removeChessman (chessman : Chessman) {
-        //chessmanList.remove(chessman)
-    }
+        //红车
+        chessboardInfo[1][1] = JuChessman(ChessType.Red, Position(1, 1))
+        chessboardInfo[1][9] = JuChessman(ChessType.Red, Position(1, 9))
+        //红马
+        chessboardInfo[1][2] = MaChessman(ChessType.Red, Position(1, 2))
+        chessboardInfo[1][8] = MaChessman(ChessType.Red, Position(1, 8))
+        //红相
+        chessboardInfo[1][3] = XiangChessman(ChessType.Red, Position(1, 3))
+        chessboardInfo[1][7] = XiangChessman(ChessType.Red, Position(1, 7))
+        //红士
+        chessboardInfo[1][4] = ShiChessman(ChessType.Red, Position(1, 4))
+        chessboardInfo[1][6] = ShiChessman(ChessType.Red, Position(1, 6))
+        //帅
+        chessboardInfo[1][5] = KingChessman(ChessType.Red, Position(1, 5))
+        //红炮
+        chessboardInfo[2][2] = PaoChessman(ChessType.Red, Position(2, 2))
+        chessboardInfo[2][8] = PaoChessman(ChessType.Red, Position(2, 8))
+        //兵
+        chessboardInfo[4][1] = PawnsChessman(ChessType.Red, Position(4, 1))
+        chessboardInfo[4][3] = PawnsChessman(ChessType.Red, Position(4, 3))
+        chessboardInfo[4][5] = PawnsChessman(ChessType.Red, Position(4, 5))
+        chessboardInfo[4][7] = PawnsChessman(ChessType.Red, Position(4, 7))
+        chessboardInfo[4][9] = PawnsChessman(ChessType.Red, Position(4, 9))
 
+        //黑车
+        chessboardInfo[10][1] = JuChessman(ChessType.Black, Position(10, 1))
+        chessboardInfo[10][9] = JuChessman(ChessType.Black, Position(10, 9))
+        //黑马
+        chessboardInfo[10][2] = MaChessman(ChessType.Black, Position(10, 2))
+        chessboardInfo[10][8] = MaChessman(ChessType.Black, Position(10, 8))
+        //黑象
+        chessboardInfo[10][3] = XiangChessman(ChessType.Black, Position(10, 3))
+        chessboardInfo[10][7] = XiangChessman(ChessType.Black, Position(10, 7))
+        //黑仕
+        chessboardInfo[10][4] = ShiChessman(ChessType.Black, Position(10, 4))
+        chessboardInfo[10][6] = ShiChessman(ChessType.Black, Position(10, 6))
+        //将
+        chessboardInfo[10][5] = KingChessman(ChessType.Black, Position(10, 5))
+        //黑炮
+        chessboardInfo[9][2] = PaoChessman(ChessType.Black, Position(9, 2))
+        chessboardInfo[9][8] = PaoChessman(ChessType.Black, Position(9, 8))
+        //卒
+        chessboardInfo[7][1] = PawnsChessman(ChessType.Black, Position(7, 1))
+        chessboardInfo[7][3] = PawnsChessman(ChessType.Black, Position(7, 3))
+        chessboardInfo[7][5] = PawnsChessman(ChessType.Black, Position(7, 5))
+        chessboardInfo[7][7] = PawnsChessman(ChessType.Black, Position(7, 7))
+        chessboardInfo[7][9] = PawnsChessman(ChessType.Black, Position(7, 9))
+
+    }
 }
